735b665ad8046e97eafdd8ae7acf14bd461180b3
PirateCoder13 <37461993+PirateCoder13@users.noreply.github.com>
2025-04-12 16:53:16 -0300

    Update Jogador.java

--- ed01/Jogador.java
+++ ed01/Jogador.java
@@ -1,44 +1,60 @@
-package ed01;
-
 /**
  * Representa um jogador do jogo de adivinhação
  */
 public class Jogador {
-    private String nome;
-    private int tentativas;
-    private int pontuacao;
+	private String nome;
+	private int tentativas;
+	private int pontuacao;
 
-    /**
-     * Construtor que inicializa o jogador com nome
-     * @param nome Nome do jogador
-     */
-    public Jogador(String nome) {
-        this.nome = nome;
-        this.tentativas = 0;
-        this.pontuacao = 0;
-    }
+	/**
+	 * Construtor que inicializa o jogador com nome
+	 * 
+	 * @param nome Nome do jogador
+	 */
+	public Jogador(String nome) {
+		this.nome = nome;
+		this.tentativas = 0;
+		this.pontuacao = 0;
+	}
 
-    // Getters e Setters
-    public String getNome() { return nome; }
-    public int getTentativas() { return tentativas; }
-    public int getPontuacao() { return pontuacao; }
-    
-    public void setNome(String nome) { this.nome = nome; }
-    public void setTentativas(int tentativas) { this.tentativas = tentativas; }
-    public void setPontuacao(int pontuacao) { this.pontuacao = pontuacao; }
+	// Getters e Setters
+	public String getNome() {
+		return nome;
+	}
 
-    /**
-     * Incrementa o número de tentativas em 1
-     */
-    public void incrementarTentativas() {
-        this.tentativas++;
-    }
+	public int getTentativas() {
+		return tentativas;
+	}
 
-    /**
-     * Adiciona pontos à pontuação do jogador
-     * @param valor Pontos a serem adicionados
-     */
-    public void atualizarPontuacao(int valor) {
-        this.pontuacao += valor;
-    }
+	public int getPontuacao() {
+		return pontuacao;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setTentativas(int tentativas) {
+		this.tentativas = tentativas;
+	}
+
+	public void setPontuacao(int pontuacao) {
+		this.pontuacao = pontuacao;
+	}
+
+	/**
+	 * Incrementa o número de tentativas em 1
+	 */
+	public void incrementarTentativas() {
+		this.tentativas++;
+	}
+
+	/**
+	 * Adiciona pontos à pontuação do jogador
+	 * 
+	 * @param valor Pontos a serem adicionados
+	 */
+	public void atualizarPontuacao(int valor) {
+		this.pontuacao += valor;
+	}
 }
